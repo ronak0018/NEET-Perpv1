e5973180216fcef8996e714f93fb77e8efd6d41d
Ronak Pandia <Ronak.Pandia@in.ey.com>
2026-05-13 19:14:39 +0530

    Allow all CORS origins for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="NEET PG Prep", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:3001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
